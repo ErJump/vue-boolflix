395172aa13dc11101efc17b38cdf4283db73fcd7
ErJump <fedelijunior@gmail.com>
2022-07-21 22:31:45 +0200

    feat: :sparkles: Adds MovieList component

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="container py-5">
     <div class="row">
-      <div class="col-6">
+      <!-- <div class="col-6">
         <h2>Movies</h2>
         <ul class="py-2" v-for="movie in moviesArray" :key="movie.id">
           <img class="w-50" :src="`${apiImgUrl}${movie.poster_path}`" :alt="movie.title">
@@ -10,7 +10,8 @@
           <li>Original Language: <span :class="`fi fi-${changeFlag(movie.original_language)}`"></span></li>
           <li>Vote Average: {{movie.vote_average}}</li>
         </ul>
-      </div>
+      </div> -->
+      <MovieList :moviesArray="moviesArray" />
       <div class="col-6">
         <h2>TV Series</h2>
         <ul class="py-2" v-for="serie in seriesArray" :key="serie.id">
@@ -21,43 +22,48 @@
           <li>Vote Average: {{serie.vote_average}}</li>
         </ul>
       </div>
+      
     </div>
   </div>
 </template>
 
 <script>
+import MovieList from './MovieList.vue';
 export default {
-  name: 'ContentMain',
-  props: {
-    moviesArray: {
-      type: Array,
-      required: true
+    name: "ContentMain",
+    props: {
+        moviesArray: {
+            type: Array,
+            required: true
+        },
+        seriesArray: {
+            type: Array,
+            required: true
+        }
     },
-    seriesArray: {
-      type: Array,
-      required: true
-    }
-  },
-  data: function () {
-    return {
-      apiImgUrl: 'https://image.tmdb.org/t/p/w780',
-    }
-  },
-  methods:{
-    changeFlag: function (lang){
-      switch (lang) {
-        case 'en':
-          return 'gb';
-        case 'ja':
-          return 'jp';
-        case 'ko':
-          return 'kr';
-        case 'cs':
-          return 'cz';
-      }
-      return lang
-    }
-  }
+    data: function () {
+        return {
+            apiImgUrl: "https://image.tmdb.org/t/p/w780",
+        };
+    },
+    methods: {
+        changeFlag: function (lang) {
+            switch (lang) {
+                case "en":
+                    return "gb";
+                case "ja":
+                    return "jp";
+                case "ko":
+                    return "kr";
+                case "cs":
+                    return "cz";
+            }
+            return lang;
+        }
+    },
+    components: { 
+      MovieList, 
+    },
 }
 </script>
 
---
+++ src/components/MovieList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="col-6">
+      <h2>Movies</h2>
+      <ul class="py-2" v-for="movie in moviesArray" :key="movie.id">
+        <img class="w-50" :src="`${apiImgUrl}${movie.poster_path}`" :alt="movie.title">
+        <li>Title: {{movie.title}}</li>
+        <li>Original Title: {{movie.original_title}}</li>
+        <li>Original Language: <span :class="`fi fi-${changeFlag(movie.original_language)}`"></span></li>
+        <li>Vote Average: {{movie.vote_average}}</li>
+      </ul>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'MovieList',
+  props:{
+    moviesArray: {
+      type: Array,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      apiImgUrl: "https://image.tmdb.org/t/p/w780",
+    }
+  },
+  methods: {
+    changeFlag: function (lang) {
+      switch (lang) {
+          case "en":
+              return "gb";
+          case "ja":
+              return "jp";
+          case "ko":
+              return "kr";
+          case "cs":
+              return "cz";
+      }
+      return lang;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
