11b17fa1d78f347c73d05b08479f5c01f683bb17
ErJump <fedelijunior@gmail.com>
2022-07-21 22:37:39 +0200

    feat: :sparkles: Adds TvSerieList component

--- src/components/ContentMain.vue
+++ src/components/ContentMain.vue
@@ -1,34 +1,15 @@
 <template>
   <div class="container py-5">
     <div class="row">
-      <!-- <div class="col-6">
-        <h2>Movies</h2>
-        <ul class="py-2" v-for="movie in moviesArray" :key="movie.id">
-          <img class="w-50" :src="`${apiImgUrl}${movie.poster_path}`" :alt="movie.title">
-          <li>Title: {{movie.title}}</li>
-          <li>Original Title: {{movie.original_title}}</li>
-          <li>Original Language: <span :class="`fi fi-${changeFlag(movie.original_language)}`"></span></li>
-          <li>Vote Average: {{movie.vote_average}}</li>
-        </ul>
-      </div> -->
       <MovieList :moviesArray="moviesArray" />
-      <div class="col-6">
-        <h2>TV Series</h2>
-        <ul class="py-2" v-for="serie in seriesArray" :key="serie.id">
-          <img class="w-50" :src="`${apiImgUrl}${serie.poster_path}`" :alt="serie.name">
-          <li>Title: {{serie.name}}</li>
-          <li>Original Title: {{serie.original_name}}</li>
-          <li>Original Language: <span :class="`fi fi-${changeFlag(serie.original_language)}`"></span></li>
-          <li>Vote Average: {{serie.vote_average}}</li>
-        </ul>
-      </div>
-      
+      <TvSerieList :seriesArray="seriesArray" />
     </div>
   </div>
 </template>
 
 <script>
 import MovieList from './MovieList.vue';
+import TvSerieList from './TvSerieList.vue';
 export default {
     name: "ContentMain",
     props: {
@@ -41,29 +22,10 @@ export default {
             required: true
         }
     },
-    data: function () {
-        return {
-            apiImgUrl: "https://image.tmdb.org/t/p/w780",
-        };
-    },
-    methods: {
-        changeFlag: function (lang) {
-            switch (lang) {
-                case "en":
-                    return "gb";
-                case "ja":
-                    return "jp";
-                case "ko":
-                    return "kr";
-                case "cs":
-                    return "cz";
-            }
-            return lang;
-        }
-    },
-    components: { 
-      MovieList, 
-    },
+    components: {
+    MovieList,
+    TvSerieList
+  },
 }
 </script>
 
---
+++ src/components/TvSerieList.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="col-6">
+        <h2>TV Series</h2>
+        <ul class="py-2" v-for="serie in seriesArray" :key="serie.id">
+          <img class="w-50" :src="`${apiImgUrl}${serie.poster_path}`" :alt="serie.name">
+          <li>Title: {{serie.name}}</li>
+          <li>Original Title: {{serie.original_name}}</li>
+          <li>Original Language: <span :class="`fi fi-${changeFlag(serie.original_language)}`"></span></li>
+          <li>Vote Average: {{serie.vote_average}}</li>
+        </ul>
+      </div>
+</template>
+
+<script>
+export default {
+  name: 'TvSerieList',
+  props:{
+    seriesArray: {
+      type: Array,
+      required: true
+    }
+  },
+  data: function () {
+    return {
+      apiImgUrl: "https://image.tmdb.org/t/p/w780",
+    }
+  },
+  methods: {
+    changeFlag: function (lang) {
+      switch (lang) {
+          case "en":
+              return "gb";
+          case "ja":
+              return "jp";
+          case "ko":
+              return "kr";
+          case "cs":
+              return "cz";
+      }
+      return lang;
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
