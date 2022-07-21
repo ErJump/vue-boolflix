9daaabfe55703f2ac61aafc49d85d83d500da96c
ErJump <fedelijunior@gmail.com>
2022-07-21 13:13:25 +0200

    feat: :sparkles: Adds getMovies

--- src/App.vue
+++ src/App.vue
@@ -8,13 +8,35 @@
 <script>
 import ContentMain from './components/ContentMain.vue'
 import HeaderMain from './components/HeaderMain.vue'
-
+import axios from 'axios';
 export default {
   name: 'App',
   components: {
     ContentMain,
     HeaderMain
-},
+  },
+  data: function () {
+    return {
+      apiKey: 'a5d177e96f7332485dbdb94d539665db',
+      apiUrlMovies: 'https://api.themoviedb.org/3/search/movie',
+      query: 'star',
+    }
+  },
+  methods:{
+    getMovies: function (){
+      console.log(`${this.apiUrlMovies}?api_key=${this.apiKey}`)
+      axios.get(`${this.apiUrlMovies}?api_key=${this.apiKey}&query=${this.query}`)
+      .then(response => {
+        console.log(response.data.results);
+      })
+      .catch(error => {
+        console.log(error);
+      })
+    }
+  },
+  created(){
+    this.getMovies();
+  }
 }
 </script>
 
