ff10b13e0201004d45e145c802afaa1ec47a307c
ErJump <fedelijunior@gmail.com>
2022-07-21 13:41:11 +0200

    feat: :sparkles: Adds SearchBar & emit to parent

--- src/components/HeaderMain.vue
+++ src/components/HeaderMain.vue
@@ -1,19 +1,42 @@
 <template>
-  <div>
-    <h1>Header</h1>
+  <div class="bg-dark py-3">
+    <div class="container">
+      <div class="row justify-content-between">
+        <div class="col-2">
+          <h1 class="text-white">Header</h1>
+        </div>
+        <div class="col-3 d-flex align-items-center">
+          <SearchBar @search="setSearch"/>
+        </div>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import SearchBar from './SearchBar.vue';
 export default {
   name: 'HeaderMain',
-  props: {
-
+  components: {
+    SearchBar,
+  },
+  data: function () {
+    return {
+      search: '',
+    }
+  },
+  methods: {
+    setSearch: function (search) {
+      this.search = search;
+      console.log(this.search);
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped lang="scss">
-
+  /* *{
+    outline: 1px solid red;
+  } */
 </style>
---
+++ src/components/SearchBar.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input v-model.trim="inputText" type="text">
+    <button @click="sendInputText">Search</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SearchBar',
+  data: function () {
+    return {
+      inputText: '',
+    }
+  },
+  methods: {
+    sendInputText: function () {
+      this.$emit('search', this.inputText);
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+
+</style>
